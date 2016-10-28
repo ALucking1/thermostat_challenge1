0baad68305189a8f65dd4b16f5dae37bae10cb55
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-28 16:31:46 +0100

    add ability to enter city for weather

--- jasmine/src/thermostatController.js
+++ jasmine/src/thermostatController.js
@@ -1,19 +1,18 @@
 $( document ).ready(function(){
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ globe +"&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json) {$('#weather').text(json.main.temp);});
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json) {$('#weather').text(json.main.temp);});
 
-
-  var thermostat = new Thermostat();
+  $("#form").submit(function(stop) {
+  stop.preventDefault();
   var globe = $( "#cities" ).val();
   $('#weather2').text("The current weather outside is...");
-  $('#cities').change(function(){
 
-    var globe = $( "#cities" ).val();
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ globe +"&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json){
         $('#weather').text(json.main.temp);
       });
     });
 
+    var thermostat = new Thermostat();
 
   $('#DecreaseTemp').click(function(){
     thermostat.downTemp();
