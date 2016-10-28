970a5a08387434468882e0f8f8463e110191d0f5
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-28 16:38:52 +0100

    user enter city

--- jasmine/src/thermostatController.js
+++ jasmine/src/thermostatController.js
@@ -1,11 +1,11 @@
 $( document ).ready(function(){
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json) {$('#weather').text(json.main.temp);});
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json) {$('#weather').text(json.main.temp);});
 
   $("#form").submit(function(stop) {
   stop.preventDefault();
   var globe = $( "#cities" ).val();
-  $('#weather2').text("The current weather outside is...");
+  $('#weather2').text("The current temperature in " + globe + " is:");
 
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ globe +"&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json){
         $('#weather').text(json.main.temp);
