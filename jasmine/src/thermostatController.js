92eee14589f0846755ba3afec0ed92acd22537b6
Julio Gonzalez <julio.gteran@gmail.com>
2016-10-28 15:10:36 +0100

    Weather API

--- jasmine/src/thermostatController.js
+++ jasmine/src/thermostatController.js
@@ -1,6 +1,19 @@
 $( document ).ready(function(){
 
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ globe +"&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json) {$('#weather').text(json.main.temp);});
+
+
   var thermostat = new Thermostat();
+  var globe = $( "#cities" ).val();
+  $('#weather2').text("The current weather outside is...");
+  $('#cities').change(function(){
+
+    var globe = $( "#cities" ).val();
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ globe +"&units=metric&APPID=a473b14fcbf6d200f1c102de58640d2f",function(json){
+        $('#weather').text(json.main.temp);
+      });
+    });
+
 
   $('#DecreaseTemp').click(function(){
     thermostat.downTemp();
